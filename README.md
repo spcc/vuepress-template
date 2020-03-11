44066d984b1024d6037c509f66e5fb0b72f83f36
7nonol <54616692+7nonol@users.noreply.github.com>
2020-03-11 11:12:35 +0800

    vue-template

---
+++ README.md
@@ -0,0 +1,15 @@
+## 开发
+```ssh
+# 克隆项目
+git clone https://github.com/PanJiaChen/vue-element-admin.git
+
+# 进入项目目录
+cd vuepress-template
+
+# 安装依赖
+npm install
+
+# 启动服务
+npm run dev
+```
+浏览器访问 http://localhost:9001
---
+++ docs/.vuepress/config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  title: '模板标题',
+  base: "/template/",
+  dest: "./template",
+  port: '9001',
+  markdown: {
+    lineNumbers: true,
+  },
+  themeConfig: {
+    sidebarDepth: 2,
+    locales: {
+      "/": {
+        sidebar: [
+          {
+            title: "测试主标题",
+            collapsable: false,
+            children: [
+              ["/views/template/", "template1"],
+              ["/views/template/", "template2"],
+            ]
+          }
+        ]
+      }
+    }
+  },
+}
